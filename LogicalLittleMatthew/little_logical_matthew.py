33a29684eca1eb2a9aa5df52819e432b55c2e328
Elysium-6 <cursed.diamondking@gmail.com>
2025-10-22 16:30:07 +0800

    prompts

--- LogicalLittleMatthew/little_logical_matthew.py
+++ LogicalLittleMatthew/little_logical_matthew.py
@@ -5,13 +5,86 @@ import random
 class LittleLogicalMatthew:
 
     def __init__(self):
-        self.chatbot_connections = [chatbot(), chatbot()]
-        self.mode = "convo"
+        self.chatbot_connections = {"analysis": chatbot(), "advice": chatbot(), "convo": chatbot()}
+        self.bot = "analysis"
         with open('prompts.json', 'r') as prompts_file:
             self.prompts= json.load(prompts_file)
+        self.initialise_chatbots()
+        self.prev = "-1"
 
-    def call_chatbot(self, message: str):
-        pass
+    def initialise_chatbots(self):
+        self.chatbot_connections["analysis"].set_preset("You are analysing user responses for important information, keywords and overall theme")
+        self.chatbot_connections["advice"].set_preset("You are a helpful assistant advising the user on career decisions")
+        self.chatbot_connections["convo"].set_preset("You are having a conversation with a friend")
+
+    def call_chatbot(self, prompt: str, bot: str):
+        return self.chatbot_connections[bot].chatbotResponse(prompt)
+
+    # def call_matthew(self, message: str):
+    #     if self.bot == "analysis":
+    #         self.analyse_response(message)
+    #     else:
+    #
+
+    def analyse_response(self, message: str):
+        category = self.call_chatbot(self.prompts['Response Analysis']['Category'] + message, "analysis")
+        print(category)
+
+        if category == "-1": return self.prompts["Conversation Template"]["Request Clarification"][random.randint(len(self.prompts["Conversation Template"]["Request Clarification"]))]
+
+        # if category == "1":
+
+        if category == "2":
+            subcat1 = self.call_chatbot(self.prompts["Response Analysis"][category]["Starter"], "analysis")
+            print(subcat1)
+            if subcat1 == "1":
+                topic = self.call_chatbot(self.prompts["Response Analysis"][category][subcat1]["Starter"], "analysis")
+                # get info using topic keyword and return string
+            if subcat1 == "2":
+                return self.call_chatbot(
+                    "Please rephrase the response to be a more natural response to the user's message but ensure you mention the keywords. Response: " +
+                    self.prompts["Response Analysis"][category][subcat1] + " User Message: " + message, "analysis")
+
+        if category == "3":
+            subcat1 = self.call_chatbot(self.prompts["Response Analysis"][category]["Starter"] + message, "analysis")
+            print(subcat1)
+            if subcat1 == "1":
+                return self.call_chatbot(self.prompts["Response Analysis"][category][subcat1] + message, "advice")
+            if subcat1 == "2":
+                return self.call_chatbot(self.prompts["Response Analysis"][category][subcat1] + message, "advice")
+            if subcat1 == "3":
+                subcat2 = self.call_chatbot(self.prompts["Response Analysis"][category][subcat1] + message, "advice")
+                if subcat2 == "1":
+                    subcat1 = "4"
+                else:
+                    return subcat2
+            if subcat1 == "4":
+                return self.call_chatbot(
+                    "Please rephrase the response to be a more natural response to the user's message but ensure you mention the keywords. Response: " +
+                    self.prompts["Response Analysis"][category][subcat1] + " User Message: " + message, "analysis")
+
+        if category == "4":
+            subcat1 = self.call_chatbot(self.prompts["Response Analysis"][category]["Starter"] + message, "analysis")
+            print(subcat1)
+            if subcat1 == "-1":
+                return self.call_chatbot(
+                    "Please rephrase the response to be a more natural response to the user's message but ensure you mention the keywords. Response: " +
+                    self.prompts["Response Analysis"][category][subcat1] + " User Message: " + message, "analysis")
+            subcat2 = self.call_chatbot(self.prompts["Response Analysis"][category]["1"]["Starter"] + message, "analysis")
+
+        if category == "5":
+            return self.call_chatbot(message, "convo")
+
+        if category == "6":
+            # Ask the AI if the user's current responses can be used to answer of the psychanalysis questions and then store the result
+            # questions = ""
+            # for framework in self.prompts["Psychoanalysis Template"].items():
+            #     for question in framework.items():
+            #         questions += question + "\n"
+            return self.call_chatbot(message, "convo")
+
+
+
 
     def initiate_convo(self):
         return (
@@ -21,5 +94,7 @@ class LittleLogicalMatthew:
 
 def test():
     test = LittleLogicalMatthew()
+    while True:
+        print(test.analyse_response(input()))
 
 test()
